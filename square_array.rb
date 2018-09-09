4a575b57ce6c3f5465bff59252c569794ff96e42
Stephen Courtier <steves0089@gmail.com>
2018-09-09 19:07:15 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,5 @@
 def square_array(array)
-  numbers_in_array = array.length
-  counter = 0
-  while counter != numbers_in_array 
-    array[counter] ** 2 
-    counter +=1 
+  for array.each do |number| 
+    number ** 2 
   end
 end
