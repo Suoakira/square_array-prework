f5150225c817286d2a4258504b97c69d289e48fc
Stephen Courtier <steves0089@gmail.com>
2018-09-09 19:05:39 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,5 +2,6 @@ def square_array(array)
   numbers_in_array = array.length
   counter = 0
   while counter != numbers_in_array 
-    array[counter] **
+    array[counter] ** 2 
+    counter +=1 
 end
